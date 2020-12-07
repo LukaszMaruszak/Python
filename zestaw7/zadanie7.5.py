838138208b6f54f07aac903269d18868db58a886
Łukasz Maruszak <64308973+LukaszMaruszak@users.noreply.github.com>
2020-12-07 18:03:40 +0100

    Add files via upload

--- zestaw7/zadanie7.5.py
+++ zestaw7/zadanie7.5.py
@@ -1,9 +1,6 @@
 from points import Point
 import unittest
-from math import sqrt
-
-PI = 3.14
-
+import math
 
 class Circle:
     """Klasa reprezentująca okręgi na płaszczyźnie."""
@@ -22,7 +19,7 @@ class Circle:
         if not isinstance(other, Circle):
             raise ValueError("Nie można porównać okręgu z czymś innym niż okrąg")
         else:
-            return self.pt.x == other.pt.x and self.pt.y == other.pt.y and self.radius == other.radius
+            return self.pt == other.pt and self.radius == other.radius
 
     def __ne__(self, other):
         if not isinstance(other, Circle):
@@ -31,25 +28,33 @@ class Circle:
 
     def area(self):
         """Obliczenie pola powierzchni koła"""
-        return PI * self.radius * self.radius
+        return math.pi * self.radius * self.radius
 
     def move(self, x, y):
         """Przesuniecie o (x, y)"""
-        self.pt.x += x
-        self.pt.y += y
-        return Circle(self.pt.x, self.pt.y, self.radius)
+        new_x = self.pt.x + x
+        new_y = self.pt.y + y
+        return Circle(new_x, new_y, self.radius)
 
     def cover(self, other):
         """Najmniejszy okrąg pokrywający oba"""
         if not isinstance(other, Circle):
             raise ValueError("Musisz podać dwa okręgi")
-        else:
-            max_radius = max(self.radius, other.radius)
-            new_radius = (sqrt((self.pt.x - other.pt.x)**2 + (self.pt.y - other.pt.y)**2))/2
-            new_radius = max_radius + new_radius
-            n_x = (abs(self.pt.x - other.pt.x)) / 2
-            n_y = (abs(self.pt.y - other.pt.y)) / 2
-            return Circle(n_x, n_y, round(new_radius, 2))
+
+        if self.pt == other.pt:
+            return Circle(self.pt.x, self.pt.y, max(self.radius, other.radius))
+
+        d = math.sqrt((other.pt.x - self.pt.x)**2 + (other.pt.y - self.pt.y)**2)
+        if self.radius > (d + other.radius):
+            #Drugie koło znajduje się w większym self
+            return self
+
+        max_radius = max(self.radius, other.radius)
+        new_radius = (math.sqrt((self.pt.x - other.pt.x)**2 + (self.pt.y - other.pt.y)**2))/2
+        new_radius = max_radius + new_radius
+        n_x = (self.pt.x + other.pt.x) / 2
+        n_y = (self.pt.y + other.pt.y) / 2
+        return Circle(n_x, n_y, round(new_radius, 2))
 
 
 # Kod testujący moduł.
@@ -88,9 +93,9 @@ class TestCircle(unittest.TestCase):
             self.assertEqual(e1.__class__, ValueError)
 
     def test_area(self):
-        self.assertEqual(self.c1.area(), 12.56)
-        self.assertEqual(self.c2.area(), 78.5)
-        self.assertEqual(self.c3.area(), 314)
+        self.assertAlmostEqual(self.c1.area(), 12.566, places=2)
+        self.assertAlmostEqual(self.c2.area(), 78.539, places=2)
+        self.assertAlmostEqual(self.c3.area(), 314.16, places=2)
 
     def test_move(self):
         self.assertEqual(self.c1.move(1, 1), Circle(2, 2, 2))
@@ -99,13 +104,17 @@ class TestCircle(unittest.TestCase):
 
     def test_cover(self):
         try:
-            self.assertEqual(Circle(0, 0, 10).cover([1, 2, 10]), Circle(2, 1.5, 12.5))
+            self.assertEqual(Circle(0, 0, 3).cover([6, -3, 4]), Circle(2, 1.5, 12.5))
         except Exception as e1:
             self.assertEqual(e1.__class__, ValueError)
 
-        self.assertEqual(Circle(0, 0, 10).cover(Circle(4, 3, 2)), Circle(2, 1.5, 12.5))
-        self.assertEqual(Circle(2, 2, 4).cover(Circle(3, 8, 2)), Circle(0.5, 3, 7.04))
-        self.assertEqual(Circle(-2, 2, 4).cover(Circle(-3, -8, 2)), Circle(0.5, 5, 9.02))
+        self.assertEqual(Circle(1, 1, 10).cover(Circle(1, 1, 2)), Circle(1, 1, 10))
+
+        self.assertEqual(Circle(0, 0, 20).cover(Circle(2, 2, 4)), Circle(0, 0, 20))
+
+        self.assertEqual(Circle(5, 5, 5).cover(Circle(0, 0, 5)), Circle(2.5, 2.5, 8.54))
+
+        self.assertEqual(Circle(-2, 2, 4).cover(Circle(-3, -8, 2)), Circle(-2.5, -3.0, 9.02))
 
 
 if __name__ == '__main__':
