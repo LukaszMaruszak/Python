7d4406b81f98b1cdbdbfb85352f19a6d060f111d
Łukasz Maruszak <64308973+LukaszMaruszak@users.noreply.github.com>
2021-01-13 01:11:49 +0100

    Kruskal algorithm

--- Projekt/Graph.py
+++ Projekt/Graph.py
@@ -1,3 +1,8 @@
+def swap(L, left, right):
+    """Zamiana miejscami dwóch elementów na liście."""
+    L[left], L[right] = L[right], L[left]
+
+
 class Edge:
     """Klasa dla krawędzi skierowanej z wagą."""
 
@@ -39,8 +44,8 @@ class Graph:
             raise ValueError("pętle są zabronione")
         if target not in self.graph[source]:
             self.graph[source][target] = weight
-        if source not in self.graph[target]:
-            self.graph[target][source] = weight
+        # if source not in self.graph[target]:
+        #     self.graph[target][source] = weight
 
     def list_nodes(self):
         """Zwraca listę wierzchołków grafu."""
@@ -54,6 +59,9 @@ class Graph:
                 L.append((source, target, self.graph[source][target]))
         return L
 
+    def graph_size(self):
+        return len(self.list_nodes())
+
     def print_graph(self):
         """Wypisuje postać grafu skierowanego ważonego na ekranie."""
         L = []
@@ -64,12 +72,39 @@ class Graph:
             L.append("\n")
         print("".join(L))
 
+    def sort_edges(self):
+        L = self.list_edges()
+        for i in range(0, len(L) - 1):
+            for j in range(0, len(L) - 1):
+                if L[j][2] > L[j + 1][2]:
+                    swap(L, j + 1, j)
+        return L
+
+    def Kruskal_Algorithm(self):
+        mst = Graph()
+        L = self.sort_edges()
+        for j in range(0, len(L)):
+            i = L.pop(0)
+            # print("Min krawędż" + str(i))
+            # Sprawdzam czy do drzewa rozpinającego dodałem wszystkie krawędzie z grafu
+
+            if i[0] not in mst.list_nodes() or i[1] not in mst.list_nodes():
+                # print("Not in mst")
+                mst.add_edge_undirected(Edge(i[0], i[1], i[2]))
+                # print(mst.list_nodes())
+                # print("in mst")
+            # print("mst size " + str(mst.graph_size() ))
+            if mst.graph_size() == self.graph_size():
+                return mst
+
+
 
 g = Graph()
-g.add_edge_undirected(Edge("A", 2, 10))
-g.add_edge_undirected(Edge(2, "B", 15))
-g.add_edge_undirected(Edge(1, 3, 11))
-
-print (g.list_nodes())
-print(g.list_edges())
-g.print_graph()
+g.add_edge_undirected(Edge("A", "B", 1))
+g.add_edge_undirected(Edge("A", "C", 2))
+g.add_edge_undirected(Edge("B", "C", 3))
+g.add_edge_undirected(Edge("C", "D", 5))
+g.add_edge_undirected(Edge("D", "B", 4))
+g.add_edge_undirected(Edge("E", "C", 7))
+g.add_edge_undirected(Edge("F", "A", 5))
+g.Kruskal_Algorithm().print_graph()
